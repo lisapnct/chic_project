153cc0d0bdd7eecaf9ce8b0936b119ac44082a5b
Lisa Poncet <poncet.lisa@gmail.com>
2020-10-16 15:05:51 +0200

    add a message and btn be the first to contribute when no contributor yet

--- client/src/components/Project/Contributors.jsx
+++ client/src/components/Project/Contributors.jsx
@@ -1,26 +1,39 @@
 import React from "react";
 
-const Contributors = (props) => {
-  return (
-    <div>
-      <h1>contributors</h1>
+class Contributors extends React.Component {
+  displayContributors = () => {
+    if (this.props.contributors.length === 0) {
+      return (
+        <div>
+          <p>No contributor yet, be the first!</p>
+          <button className="button is-primary">contribute now</button>
+        </div>
+      );
+    } else {
+      return this.props.contributors.map((contributor) => (
+        <div key={contributor._id}>
+          <h2>Name: {contributor.id_users[0].userName}</h2>
+          <p>
+            contributed with:
+            {contributor.contributed_materials.map((material) => (
+              <span key={material.fabric_type}>
+                {material.quantity} piece(s) of {material.fabric_type}
+              </span>
+            ))}
+          </p>
+        </div>
+      ));
+    }
+  };
 
-      {props.contributors &&
-        props.contributors.map((contributor) => (
-          <div key={contributor._id}>
-            <h2>Name: {contributor.id_users[0].userName}</h2>
-            <p>
-              contributed with:
-              {contributor.contributed_materials.map((material) => (
-                <span key={material.fabric_type}>
-                  {material.quantity} piece(s) of {material.fabric_type}
-                </span>
-              ))}
-            </p>
-          </div>
-        ))}
-    </div>
-  );
-};
+  render() {
+    return (
+      <div>
+        <h1>contributors</h1>
+        {this.props.contributors && this.displayContributors()}
+      </div>
+    );
+  }
+}
 
 export default Contributors;
